13a48d96f92966d886fcc460bd000b01452cbd42
Kim, Joo-Ho <teamsprint@gmail.com>
2017-06-08 02:22:07 +0900

    twitter search done

--- src/main/java/kr/co/hanbit/mastering/MasteringSpringMvc4Application.java
+++ src/main/java/kr/co/hanbit/mastering/MasteringSpringMvc4Application.java
@@ -1,19 +1,13 @@
 package kr.co.hanbit.mastering;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-//@ComponentScan(basePackageClasses = HelloController.class)
 public class MasteringSpringMvc4Application {
-//	@Autowired
-//	private ErrorAttributes errorAttributes;
-//
-//	@Bean
-//	public BasicErrorController basicErrorController(){
-//		return new BasicErrorController(errorAttributes, null);
-//	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(MasteringSpringMvc4Application.class, args);
 	}
--- src/main/java/kr/co/hanbit/mastering/controller/HelloController.java
+++ src/main/java/kr/co/hanbit/mastering/controller/HelloController.java
@@ -1,18 +1,38 @@
 package kr.co.hanbit.mastering.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.twitter.api.SearchResults;
+import org.springframework.social.twitter.api.Tweet;
+import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by jhkim on 2017. 6. 7..
  */
 @Controller
 public class HelloController {
+    private static Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+
+    @SuppressWarnings("SpringJavaAutowiringInspection")
+    @Autowired
+    private Twitter twitter;
+
     @RequestMapping("/")
-    public String hello(@RequestParam(defaultValue = "world!") String userName, Model model) {
-        model.addAttribute("message", "Hello " + userName);
+    public String hello(@RequestParam(defaultValue = "teamsprint") String search, Model model) {
+        SearchResults searchResults = twitter.searchOperations().search(search);
+        List<String> tweets = searchResults.getTweets().stream().map(Tweet::getText).collect(Collectors.toList());
+//        String text = searchResults.getTweets().get(0).getText();
+//        LOGGER.debug(text);
+        model.addAttribute("tweets", tweets);
+//        model.addAttribute("message", text);
         return "resultPage";
     }
 }

3225a43b1927940f4f636f1f7614b13c43c6656a
Kim, Joo-Ho <teamsprint@gmail.com>
2017-06-08 01:35:12 +0900

    using request param

--- src/main/java/kr/co/hanbit/mastering/controller/HelloController.java
+++ src/main/java/kr/co/hanbit/mastering/controller/HelloController.java
@@ -3,6 +3,7 @@ package kr.co.hanbit.mastering.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by jhkim on 2017. 6. 7..
@@ -10,8 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
     @RequestMapping("/")
-    public String hello(Model model) {
-        model.addAttribute("message", "Hello from the controller");
+    public String hello(@RequestParam("name") String userName, Model model) {
+        model.addAttribute("message", "Hello " + userName);
         return "resultPage";
     }
 }

4e4e2ada3b5f5cbd1436d6dcd5b2cf02d18b66d8
Kim, Joo-Ho <teamsprint@gmail.com>
2017-06-08 01:33:19 +0900

    using ${"message"}

--- src/main/java/kr/co/hanbit/mastering/controller/HelloController.java
+++ src/main/java/kr/co/hanbit/mastering/controller/HelloController.java
@@ -1,6 +1,7 @@
 package kr.co.hanbit.mastering.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -9,12 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
     @RequestMapping("/")
-    public String hello() {
+    public String hello(Model model) {
+        model.addAttribute("message", "Hello from the controller");
         return "resultPage";
     }
-
-//    @RequestMapping("/error")
-//    public String onError() {
-//        return null;
-//    }
 }

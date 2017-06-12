ab90c62374017c250b1360301fbd16be093309d1
Kim, Joo-Ho <teamsprint@gmail.com>
2017-06-12 22:17:44 +0900

    using POST to redirect

--- src/main/java/kr/co/hanbit/mastering/controller/TweetController.java
+++ src/main/java/kr/co/hanbit/mastering/controller/TweetController.java
@@ -9,8 +9,11 @@ import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
@@ -36,5 +39,16 @@ public class TweetController {
         model.addAttribute("tweets", tweets);
         model.addAttribute("search", search);
         return "resultPage";
+    }
+
+    @RequestMapping(value = "/postSearch", method = RequestMethod.POST)
+    public String postSearch(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+        String search = request.getParameter("search");
+        if (search.toLowerCase().contains("struts")) {
+            redirectAttributes.addFlashAttribute("error", "Try using spring instead!");
+            return "redirect:/";
+        }
+        redirectAttributes.addAttribute("search", search);
+        return "redirect:result";
     }
 }
